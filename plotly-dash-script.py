1123f9e2ebeafca8bd74b3c0f0e305fb4fe04d26
srpatel2000 <siddhip2009@gmail.com>
2021-05-11 01:33:33 -0700

    Added line chart

--- plotly-dash-script.py
+++ plotly-dash-script.py
@@ -1,4 +1,4 @@
-# import statements
+# IMPORT STATEMENTS
 
 import pandas as pd
 import plotly.express as px  # (version 4.7.0)
@@ -12,15 +12,64 @@ from dash.dependencies import Input, Output
 app = dash.Dash(__name__)
 
 # -------------------------------------------------------------------------------------
+# READ IN DATA
+top_songs = pd.read_csv('top_songs.csv')
+
+# -------------------------------------------------------------------------------------
 # APP LAYOUT 
 
 app.layout = html.Div([
 
     html.H1("Spotify Top Songs Dashboard (DSC 106)", style={'text-align': 'center'}),
 
+    dcc.Checklist(
+        id='my_checklist',
+        options = [
+            {'label':'Danceability', 'value':'danceability'},
+            {'label':'Energy', 'value':'energy'},
+            {'label':'Key', 'value':'key'},
+            {'label':'Loudness', 'value':'loudness'},
+            {'label':'Mode', 'value':'mode'},
+            {'label':'Speechiness', 'value':'speechiness'},
+            {'label':'Acousticness', 'value':'acousticness'},
+            {'label':'Instrumentalness', 'value':'instrumentalness'},
+            {'label':'Liveness', 'value':'liveness'},
+            {'label':'Valence', 'value':'valence'},
+            {'label':'Tempo', 'value':'tempo'},
+            {'label':'Duration', 'value':'duration'},
+            {'label':'Time Signature', 'value':'time_signature'},
+        ],  value =['danceability']),
+
+         html.Div([
+            dcc.Graph(id='line_graph')
+        ]),
+
 ])
 
 # -------------------------------------------------------------------------------------
+# CONNECT THE PLOTLY GRAPHS WITH DASH COMPONENTS 
+@app.callback(
+    Output(component_id='line_graph', component_property='figure'),
+    [Input(component_id='my_checklist', component_property='value')]
+)
 
+def update_line_graph(selected_columns):
+    line_copy = top_songs.copy()
+
+    columns = selected_columns +  ['year']
+
+    line_copy = line_copy[columns]
+    line_copy = top_songs.groupby('year', as_index=False).mean()
+
+    fig = px.line(
+        data_frame = line_copy,
+        x = 'year', 
+        y = selected_columns
+    )
+
+    return fig
+
+# -------------------------------------------------------------------------------------
+# RUN THE APP 
 if __name__ == '__main__':
     app.run_server(debug=True)
