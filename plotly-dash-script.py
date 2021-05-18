90186275f53f051175ec59126f21ec18e1a51263
srpatel2000 <siddhip2009@gmail.com>
2021-05-17 23:54:11 -0700

    Added sidebar + more formatting

--- plotly-dash-script.py
+++ plotly-dash-script.py
@@ -24,73 +24,104 @@ top_songs = pd.read_csv('top_songs.csv')
 # -------------------------------------------------------------------------------------
 # APP LAYOUT 
 
+sidebar = html.Div(
+    [
+        html.H3("Spotify Top Songs Dashboard", className="display-4", style = {"font-size": "30px", "font-weight": "600"}),
+        # button
+        html.A(html.Button("Renaldy Herlim", style={'background-color': '#212121', 'border': '2px solid', 'border-radius': '10px', 'font-family':"Montserrat", 'text-align': 'center', "font-weight": "600"}), href='https://www.linkedin.com/in/renaldy-herlim/', target='_blank'),
+        html.A(html.Button("Siddhi Patel", style={'background-color': '#212121', 'border': '2px solid', 'border-radius': '10px', 'font-family':"Montserrat", 'text-align': 'center', "font-weight": "600"}), href='https://www.linkedin.com/in/siddhipatel-stu/', target='_blank'),
+        html.Hr(),
+        html.P(
+            "This dashboard was created for the SP'21 iteration of DSC106. It conveys different information about top songs throughout the years on Spotify.", className="lead", style = {"font-size": "15px"}
+        ),
+        html.Hr(),
+        dbc.Nav(
+            [
+                dbc.NavLink("Goals", href="/", active="exact"),
+                dbc.NavLink("Audio Features Over the Years", href="/page-1", active="exact"),
+                dbc.NavLink("Top Genres Throughout the Years", href="/page-2", active="exact"),
+                dbc.NavLink("Top Artists Throughout the Years", href="/page-3", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+        html.Iframe(src = "https://open.spotify.com/embed/playlist/37i9dQZF1DWVRSukIED0e9", style={"width":"320px", "height":"80px", "shadow":"none"}, allow="encrypted-media")
+    ],
+    style={
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "20rem",
+    "padding": "2rem 1rem",
+    "background-color": "black",
+    },
+)
+
+content = html.Div(id="page-content", children=[], style={"margin-left": "22rem", "margin-right": "2rem", "margin-top":"3rem", "padding": "5rem 1rem"})
+
 app.layout = html.Div([
 
-    # title 
-    html.H1("Spotify Top Songs Dashboard", style={'text-align': 'center', 'color': '#1db954', 'font-family':"Montserrat"}),
-    # subheading
-    html.H3("DSC 106 Final Project", style={'text-align': 'center', 'color': '#1db954', 'font-family':"Montserrat"}),
-    # button
-    # html.A(html.Button("Renaldy Herlim", style={'color': 'dark gray 2', 'font-family':"Montserrat"}), href='https://www.linkedin.com/in/siddhipatel-stu/'),
-    # html.A(html.Button("Siddhi Patel", style={'color': 'dark gray 2', 'font-family':"Montserrat"}), href='https://www.linkedin.com/in/siddhipatel-stu/'),
+    dcc.Location(id="url"),
 
-    # line graph checklist
-    dcc.Checklist(
-        style={'color': 'white'},
-        id='my_checklist',
-        options = [
-            {'label':'Danceability', 'value':'danceability'},
-            {'label':'Energy', 'value':'energy'},
-            {'label':'Key', 'value':'key'},
-            {'label':'Loudness', 'value':'loudness'},
-            {'label':'Mode', 'value':'mode'},
-            {'label':'Speechiness', 'value':'speechiness'},
-            {'label':'Acousticness', 'value':'acousticness'},
-            {'label':'Instrumentalness', 'value':'instrumentalness'},
-            {'label':'Liveness', 'value':'liveness'},
-            {'label':'Valence', 'value':'valence'},
-            {'label':'Tempo', 'value':'tempo'},
-            {'label':'Duration', 'value':'duration'},
-            {'label':'Time Signature', 'value':'time_signature'},
-        ],  value =['danceability']),
+    # # line graph checklist
+    # dcc.Checklist(
+    #     style={'color': 'white', 'margin-top': '10px'},
+    #     id='my_checklist',
+    #     options = [
+    #         {'label':'Danceability', 'value':'danceability'},
+    #         {'label':'Energy', 'value':'energy'},
+    #         {'label':'Key', 'value':'key'},
+    #         {'label':'Loudness', 'value':'loudness'},
+    #         {'label':'Mode', 'value':'mode'},
+    #         {'label':'Speechiness', 'value':'speechiness'},
+    #         {'label':'Acousticness', 'value':'acousticness'},
+    #         {'label':'Instrumentalness', 'value':'instrumentalness'},
+    #         {'label':'Liveness', 'value':'liveness'},
+    #         {'label':'Valence', 'value':'valence'},
+    #         {'label':'Tempo', 'value':'tempo'},
+    #         {'label':'Duration', 'value':'duration'},
+    #         {'label':'Time Signature', 'value':'time_signature'},
+    #     ],  value =['danceability']),
 
-        # testing branch
+    #     # line graph
+    #     html.Div([
+    #         dcc.Graph(id='line_graph')
+    #     ]),
 
-        # line graph
-        html.Div([
-            dcc.Graph(id='line_graph')
-        ]),
+        # # genres bar chart title
+        # html.Div([
+        #  dcc.Graph(id='genres_graph')
+        # ]),
 
-    # genres bar chart title
-    html.Div([
-        dcc.Graph(id='genres_graph')
-    ]),
-
-    # genres bar chart slider
-    dcc.Slider(
-    id='genres_year_slider',
-    min=top_songs['year'].min(),
-    max=top_songs['year'].max(),
-    value=top_songs['year'].min(),
-    marks={str(year): str(year) for year in top_songs['year'].unique()},
-    step=None
-    ),
+        # # genres bar chart slider
+        # dcc.Slider(
+        # id='genres_year_slider',
+        # min=top_songs['year'].min(),
+        # max=top_songs['year'].max(),
+        # value=top_songs['year'].min(),
+        # marks={str(year): str(year) for year in top_songs['year'].unique()},
+        # step=None
+        # ),
 
 
-    # artists bar chart title
-    html.Div([
-        dcc.Graph(id='artists_graph')
-    ]),
+        # # artists bar chart title
+        # html.Div([
+        #     dcc.Graph(id='artists_graph')
+        # ]),
 
-    # artists bar chart slider
-    dcc.Slider(
-    id='artists_year_slider',
-    min=top_songs['year'].min(),
-    max=top_songs['year'].max(),
-    value=top_songs['year'].min(),
-    marks={str(year): str(year) for year in top_songs['year'].unique()},
-    step=None
-    ),
+        # # artists bar chart slider
+        # dcc.Slider(
+        # id='artists_year_slider',
+        # min=top_songs['year'].min(),
+        # max=top_songs['year'].max(),
+        # value=top_songs['year'].min(),
+        # marks={str(year): str(year) for year in top_songs['year'].unique()},
+        # step=None
+        # ),
+
+        sidebar,
+        content
 
 ])
 
@@ -235,6 +266,92 @@ def update_artists_graph(year_val):
     })
 
     return fig
+
+# CONTENT CALLBACK
+@app.callback(
+    Output("page-content", "children"),
+    [Input("url", "pathname")]
+)
+
+def render_page_content(pathname):
+    if pathname == "/":
+        return [
+                # html.H1('Kindergarten in Iran',
+                #         style={'textAlign':'center'}),
+                # line graph checklist
+            dcc.Checklist(
+                style={'color': 'white', 'margin-top': '10px'},
+                id='my_checklist',
+                options = [
+                    {'label':'Danceability', 'value':'danceability'},
+                    {'label':'Energy', 'value':'energy'},
+                    {'label':'Key', 'value':'key'},
+                    {'label':'Loudness', 'value':'loudness'},
+                    {'label':'Mode', 'value':'mode'},
+                    {'label':'Speechiness', 'value':'speechiness'},
+                    {'label':'Acousticness', 'value':'acousticness'},
+                    {'label':'Instrumentalness', 'value':'instrumentalness'},
+                    {'label':'Liveness', 'value':'liveness'},
+                    {'label':'Valence', 'value':'valence'},
+                    {'label':'Tempo', 'value':'tempo'},
+                    {'label':'Duration', 'value':'duration'},
+                    {'label':'Time Signature', 'value':'time_signature'},
+                ],  value =['danceability']),
+
+                # line graph
+                html.Div([
+                    dcc.Graph(id='line_graph')
+                ])
+        ]
+
+    elif pathname == "/page-1":
+        return [
+                # html.H1('Grad School in Iran',
+                #         style={'textAlign':'center'}),
+                # genres bar chart title
+                html.Div([
+                dcc.Graph(id='genres_graph')
+                ]),
+
+                # genres bar chart slider
+                dcc.Slider(
+                id='genres_year_slider',
+                min=top_songs['year'].min(),
+                max=top_songs['year'].max(),
+                value=top_songs['year'].min(),
+                marks={str(year): str(year) for year in top_songs['year'].unique()},
+                step=None
+                )
+                ]
+
+    elif pathname == "/page-2":
+        return [
+                # html.H1('High School in Iran',
+                #         style={'textAlign':'center'}),
+                # artists bar chart title
+                html.Div([
+                    dcc.Graph(id='artists_graph')
+                ]),
+
+                # artists bar chart slider
+                dcc.Slider(
+                id='artists_year_slider',
+                min=top_songs['year'].min(),
+                max=top_songs['year'].max(),
+                value=top_songs['year'].min(),
+                marks={str(year): str(year) for year in top_songs['year'].unique()},
+                step=None
+                ),
+        ]
+
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
 # -------------------------------------------------------------------------------------
 # RUN THE APP 
 if __name__ == '__main__':
