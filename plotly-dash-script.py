addcb70f7c65c93164171707f7280194663504be
Renaldy Herlim <renaldyh27@gmail.com>
2021-05-18 15:34:18 -0700

    heroku deploy

--- plotly-dash-script.py
+++ plotly-dash-script.py
@@ -15,7 +15,7 @@ from dash.dependencies import Input, Output
 
 
 app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP], suppress_callback_exceptions = True)
-server = app.server
+server=app.server
 
 # -------------------------------------------------------------------------------------
 # READ IN DATA
